fafaac2bdac145acc640a85d39ad4716a94a65d1
Bob Tierney <btierney@redhat.com>
2016-03-28 11:18:34 -0500

    chart layout

--- www/scripts/chartsummary.js
+++ www/scripts/chartsummary.js
@@ -58,7 +58,10 @@ function plotGraph(categories, data) {
             series: {
                 states: {
                     hover: {
-                        enabled: false
+                        enabled: false,
+                        halo : {
+                            enabled: false;
+                        }
                     }
                 }
             }
